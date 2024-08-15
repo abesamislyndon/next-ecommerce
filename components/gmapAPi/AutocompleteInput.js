f53234f3498cb3fde77948fbb89bba5c4715f75c
dondon <abesamislyndon@gmail.com>
2024-08-15 09:02:02 +0800

    key hide

--- components/gmapAPi/AutocompleteInput.js
+++ components/gmapAPi/AutocompleteInput.js
@@ -4,7 +4,7 @@ import { useLoadScript } from "@react-google-maps/api";
   const libraries = ["places"];
   const AutocompleteInput = ({ value, onChangeadd, address_info }) => {
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "AIzaSyDBd9wUXnlHTyDuVrtyfv9-AZTFSL3Vq58",
+    googleMapsApiKey: "nothing-to-hide",
     libraries,
   });
 
